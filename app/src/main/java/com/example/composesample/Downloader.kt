5d1b76f6b366c74ca6afc3140dba10a424706f4b
Chen Chen <talent_prince@163.com>
2022-04-27 16:56:52 +0800

    Create downloader logic

--- app/src/main/java/com/example/composesample/Downloader.kt
+++ app/src/main/java/com/example/composesample/Downloader.kt
@@ -1,7 +1,10 @@
 package com.example.composesample
 
+import android.annotation.SuppressLint
+import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Context
+import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
 import java.net.URL
@@ -9,23 +12,50 @@ import java.net.URL
 class Downloader(private val context: Context) {
 
     fun download(url: String, fileName: String) {
-        saveFileUsingMediaStore(context, url, fileName)
+        saveFileUsingMediaStore(url, fileName)
     }
 
-    private fun saveFileUsingMediaStore(context: Context, url: String, fileName: String) {
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
-            put(MediaStore.MediaColumns.MIME_TYPE, "pdf")
-            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+    @SuppressLint("Range")
+    private fun saveFileUsingMediaStore(url: String, fileName: String) {
+        val uri = fileName.run {
+            existFileUri() ?: newFileUri() ?: throw IllegalStateException("Can't create file uri")
         }
-        val resolver = context.contentResolver
-        val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
-        if (uri != null) {
-            URL(url).openStream().use { input ->
-                resolver.openOutputStream(uri).use { output ->
-                    input.copyTo(output!!, DEFAULT_BUFFER_SIZE)
-                }
+
+        URL(url).openStream().use { input ->
+            context.contentResolver.openOutputStream(uri, "rwt").use { output ->
+                input.copyTo(output!!, DEFAULT_BUFFER_SIZE)
             }
         }
     }
+
+    private fun String.newFileUri(): Uri? {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, this@newFileUri)
+            put(MediaStore.MediaColumns.MIME_TYPE, "png")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+        }
+        val resolver = context.contentResolver
+        return resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+    }
+
+    @SuppressLint("Range")
+    private fun String.existFileUri(): Uri? {
+        val contentUri = MediaStore.Files.getContentUri("external")
+        val selection = MediaStore.MediaColumns.RELATIVE_PATH + "=?"
+        val selectionArgs = arrayOf("${Environment.DIRECTORY_DOWNLOADS}/")
+        val cursor = context.contentResolver.query(contentUri, null, selection, selectionArgs, null)
+        if (cursor != null && cursor.count != 0) {
+            while (cursor.moveToNext()) {
+                val name =
+                    cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME))
+                if (this == name) {
+                    val id = cursor.getLong(cursor.getColumnIndex(MediaStore.MediaColumns._ID))
+                    return ContentUris.withAppendedId(contentUri, id);
+                }
+            }
+        }
+        return null
+    }
 }
+
+
--- app/src/main/java/com/example/composesample/MainViewModel.kt
+++ app/src/main/java/com/example/composesample/MainViewModel.kt
@@ -13,14 +13,13 @@ class MainViewModel @Inject constructor(private val downloader: Downloader) :
 
     data class State(
         val loading: Boolean = false,
-        val progress: Float = 0f,
     )
 
     fun saveFile(url: String) {
         viewModelScope.launch {
             updateState { copy(loading = true) }
             withContext(Dispatchers.IO) {
-                downloader.download(url, "test_file")
+                downloader.download(url, "test_file.png")
             }
             updateState { copy(loading = false) }
         }
